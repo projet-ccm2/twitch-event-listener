918f671309dd445075e95b0ef602d4e86dd5b92e
Maxcwt <148951047+Maxcwt@users.noreply.github.com>
2025-11-29 09:59:31 +0100

    Add logger middleware and metrics controller for Twitch Listener Microservice

---
+++ src/middlewares/loggerMiddleware.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from 'express';
+import { logger } from '../utils/logger';
+
+
+export const loggerMiddleware = (
+    req: Request,
+    _res: Response,
+    next: NextFunction
+) => {
+    logger.info(`${req.method} ${req.url}`);
+    next();
+};
